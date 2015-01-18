0ae0232c8c50886b88f313ae2d5a53be6b60f255
Andres Garcia <andres.garbio@gmail.com>
2015-01-17 22:44:20 -0600

    Comentario Robot Map

--- Drive/src/org/usfirst/frc/team4403/robot/RobotMap.java
+++ Drive/src/org/usfirst/frc/team4403/robot/RobotMap.java
@@ -1,11 +1,12 @@
 package org.usfirst.frc.team4403.robot;
 
 public class RobotMap {
-    
+	//PWM
     public static int leftMotor = 0;
     public static int rightMotor = 1;
-    public static int joystickPort = 0;
     public static int leftRoller = 2;
     public static int rightRoller = 3;
+    //Puertos 
+    public static int joystickPort = 0;
     
 }
